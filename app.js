dc31c5c9441e847a98667308da3b820debadd322
Rupesh2021 <rupeshksingh5@gmail.com>
2020-12-30 10:00:42 +0530

    nodejs project first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Testing for SSH file movement to GitLab</h1>')
+})
+
+app.listen(8080,()=>{
+    console.log(`The server is running at port 8080`);
+})
